290e11425ac02c2dc2643a1755b32081da399626
googlecode@antlersoft.com <googlecode@antlersoft.com@9a6f56fa-734f-0410-9475-d36a37710cfd>
2009-01-19 23:24:41 +0000

    Branch for changes that will be dependent on sqlitegen eclipse plug-in

---
+++ branches/antlersoft/androidVNC/src/android/androidVNC/VncConstants.java
@@ -0,0 +1,13 @@
+package android.androidVNC;
+
+/**
+ * Keys for intent values
+ */
+public class VncConstants {
+	public static final String ID = "ID";
+	public static final String PASSWORD = "PASSWORD";
+	public static final String PORT = "PORT";
+	public static final String IP = "IP";
+	public static final String HOST = "HOST";
+	public static final String COLORMODEL = "COLORMODEL";
+}
