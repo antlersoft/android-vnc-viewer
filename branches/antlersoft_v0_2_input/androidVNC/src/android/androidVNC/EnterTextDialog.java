ebe58405531bd81d28fab2555b728c8de09a5eee
googlecode@antlersoft.com <googlecode@antlersoft.com@9a6f56fa-734f-0410-9475-d36a37710cfd>
2009-09-29 21:29:26 +0000

    Keeps history of entered text (not in database though; only lasts for one session)

--- branches/antlersoft_v0_2_input/androidVNC/src/android/androidVNC/EnterTextDialog.java
+++ branches/antlersoft_v0_2_input/androidVNC/src/android/androidVNC/EnterTextDialog.java
@@ -5,6 +5,8 @@ package android.androidVNC;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
@@ -13,6 +15,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * @author Michael A. MacDonald
@@ -23,11 +26,20 @@ class EnterTextDialog extends Dialog {
 	private VncCanvasActivity _canvasActivity;
 	
 	private EditText _textEnterText;
+	
+	private ArrayList<String> _history;
+	
+	private int _historyIndex;
+	
+	private ImageButton _buttonNextEntry;
+	private ImageButton _buttonPreviousEntry;
 
 	public EnterTextDialog(Context context) {
 		super(context);
 		setOwnerActivity((Activity)context);
 		_canvasActivity = (VncCanvasActivity)context;
+		_history = new ArrayList<String>();
+		_historyIndex = 0;
 	}
 
 	/* (non-Javadoc)
@@ -39,6 +51,43 @@ class EnterTextDialog extends Dialog {
 		setContentView(R.layout.entertext);
 		setTitle(R.string.enter_text_title);
 		_textEnterText = (EditText)findViewById(R.id.textEnterText);
+		_buttonNextEntry = (ImageButton)findViewById(R.id.buttonNextEntry);
+		_buttonNextEntry.setOnClickListener(new View.OnClickListener() {
+
+			/* (non-Javadoc)
+			 * @see android.view.View.OnClickListener#onClick(android.view.View)
+			 */
+			@Override
+			public void onClick(View v) {
+				if (_historyIndex < _history.size())
+					_historyIndex++;
+				if (_historyIndex < _history.size())
+				{
+				    _textEnterText.setText(_history.get(_historyIndex));
+				}
+				else
+				{
+					_textEnterText.setText("");
+				}
+				updateButtons();
+			}
+			
+		});
+		_buttonPreviousEntry = (ImageButton)findViewById(R.id.buttonPreviousEntry);
+		_buttonPreviousEntry.setOnClickListener(new View.OnClickListener() {
+
+			/* (non-Javadoc)
+			 * @see android.view.View.OnClickListener#onClick(android.view.View)
+			 */
+			@Override
+			public void onClick(View v) {
+				if (_historyIndex > 0)
+					_historyIndex--;
+			    _textEnterText.setText(_history.get(_historyIndex));
+				updateButtons();
+			}
+			
+		});
 		((Button)findViewById(R.id.buttonSendText)).setOnClickListener(new View.OnClickListener() {
 
 			/* (non-Javadoc)
@@ -71,9 +120,27 @@ class EnterTextDialog extends Dialog {
 						// TODO: log this
 					}
 				}
+				_textEnterText.setText("");
+				_history.add(s);
+				_historyIndex = _history.size();
+				updateButtons();
 				dismiss();
 			}
 			
 		});
+	}
+
+	private void updateButtons()
+	{
+		_buttonPreviousEntry.setEnabled(_historyIndex > 0);
+		_buttonNextEntry.setEnabled(_historyIndex <_history.size());
+	}
+	
+	/* (non-Javadoc)
+	 * @see android.app.Dialog#onStart()
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
 	}
 }
