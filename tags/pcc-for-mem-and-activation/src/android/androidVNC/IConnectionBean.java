f16264ddb38682343b19380108139bb59c696e48
googlecode@antlersoft.com <googlecode@antlersoft.com@9a6f56fa-734f-0410-9475-d36a37710cfd>
2010-03-04 17:35:26 +0000

    Mark this point in case we need to revert these

---
+++ tags/pcc-for-mem-and-activation/src/android/androidVNC/IConnectionBean.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2009 Michael A. MacDonald
+ */
+package android.androidVNC;
+
+import com.antlersoft.android.db.*;
+
+/**
+ * @author Michael A. MacDonald
+ *
+ */
+@TableInterface(ImplementingClassName="AbstractConnectionBean",TableName="CONNECTION_BEAN")
+interface IConnectionBean {
+	@FieldAccessor
+	long get_Id();
+	@FieldAccessor
+	String getNickname();
+	@FieldAccessor
+	String getAddress();
+	@FieldAccessor
+	int getPort();
+	@FieldAccessor
+	String getPassword();
+	@FieldAccessor
+	String getColorModel();
+	@FieldAccessor
+	boolean getForceFull();
+	@FieldAccessor
+	String getRepeaterId();
+	@FieldAccessor
+	String getInputMode();
+	@FieldAccessor(Name="SCALEMODE")
+	String getScaleModeAsString();
+	@FieldAccessor
+	boolean getUseLocalCursor();
+	@FieldAccessor
+	boolean getKeepPassword();
+	@FieldAccessor
+	boolean getFollowMouse();
+	@FieldAccessor
+	boolean getUseRepeater();
+	@FieldAccessor
+	long getMetaListId();
+	@FieldAccessor(Name="LAST_META_KEY_ID")
+	long getLastMetaKeyId();
+	@FieldAccessor(DefaultValue="0")
+	boolean getFollowPan();
+}
