a75b1dcddefee3adbd854746aa103de5fc2d5ee0
googlecode@antlersoft.com <googlecode@antlersoft.com@9a6f56fa-734f-0410-9475-d36a37710cfd>
2010-07-04 22:52:46 +0000

    Change shortcut to encode connection in a URI instead of a Bundle; bundle is not working when app is not started

--- branches/antlersoft/androidVNC/src/android/androidVNC/VncConstants.java
+++ branches/antlersoft/androidVNC/src/android/androidVNC/VncConstants.java
@@ -4,5 +4,5 @@ package android.androidVNC;
  * Keys for intent values
  */
 public class VncConstants {
-	public static final String CONNECTION = "CONNECTION";
+	public static final String CONNECTION = "android.androidVNC.CONNECTION";
 }
