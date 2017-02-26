6c249a9becdae79ab3ba2378896856e2b13b7989
Michael A. MacDonald <mars@antlersoft.com>
2017-02-26 17:16:23 -0500

    Make ant build work with current ant build.xml
    Where current is descibed as version 14, or after Android 2.3

--- ZoomerWithKeys/gen/com/antlersoft/android/zoomer/BuildConfig.java
+++ ZoomerWithKeys/gen/com/antlersoft/android/zoomer/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.antlersoft.android.zoomer;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
--- androidVNC/gen/android/androidVNC/BuildConfig.java
+++ androidVNC/gen/android/androidVNC/BuildConfig.java
@@ -2,5 +2,5 @@
 package android.androidVNC;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
